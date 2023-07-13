cb7536531e1256b68a00fb382c298da908415ed6
ch-und | NodeValidator <batuoc263@hotmail.com>
2023-07-13 09:15:33 +0700

    Update contract

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,18 +2,18 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Goerli } from "@thirdweb-dev/chains";
-export const NETWORK = Goerli;
+import { ZksyncEra } from "@thirdweb-dev/chains";
+export const NETWORK = ZksyncEra;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x8709A34cC95188eD647BEe825a0a39034D91E763";
+// Rewiki Marketplace
+export const MARKETPLACE_ADDRESS = "0xDD39DBcc155EfC5F2fc4655347B8734c0D4C1363";
 
-// 3. The address of your NFT collection smart contract.
+// 3. The address of your NFT collection smart contract. 
+// Rewiki on ZKSync
 export const NFT_COLLECTION_ADDRESS =
-  "0x8C67beBF64E029dB3B4bf5d252951CD8bd5dE9b5";
+  "0xbBB3C56f0Ee015990e86A44a5FB5a8d33462200c";
 
 // (Optional) Set up the URL of where users can view transactions on
-// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-// export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
-export const ETHERSCAN_URL = "https://goerli.etherscan.io";
+export const ETHERSCAN_URL = "https://explorer.zksync.io";
